a3f33c577e24cc4c349547b366ddc173e35f8918
slymgz345-web <slymgz345@gmail.com>
2026-05-11 22:36:44 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
-<div align="center">
-<img width="1200" height="475" alt="GHBanner" src="https://github.com/user-attachments/assets/0aa67016-6eaf-458a-adb2-6e31a0763ed6" />
-</div>
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-# Run and deploy your AI Studio app
-
-This contains everything you need to run your app locally.
-
-View your app in AI Studio: https://ai.studio/apps/1bb12ae3-7fdb-433d-a50a-2879dcdcb26e
-
-## Run Locally
-
-**Prerequisites:**  Node.js
-
-
-1. Install dependencies:
-   `npm install`
-2. Set the `GEMINI_API_KEY` in [.env.local](.env.local) to your Gemini API key
-3. Run the app:
-   `npm run dev`
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
