88ff9f71ac8b1489add151931a75e12d4b6259aa
slymgz345-web <slymgz345@gmail.com>
2026-05-12 20:22:37 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// Bu satır Vercel'in beklediği "export" işlemini yapar:
+export default defineConfig({
+  plugins: [react()],
+})
