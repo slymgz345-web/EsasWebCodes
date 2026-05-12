5876a4466fdfa95ac318d7fb70aa5ece1b9d073e
slymgz345-web <slymgz345@gmail.com>
2026-05-12 22:07:04 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyan-custom': '#22d3ee',
+        'blue-custom': '#3b82f6',
+        'purple-custom': '#9333ea',
+        'dark-bg': '#050608',
+        'dark-border': '#0d0f14',
+      }
+    },
   },
   plugins: [],
 }
